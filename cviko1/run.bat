43500cf2dd1d5a17a6822943dba8f84a556f8bc1
Robert Belanec <belanecrobert22@gmail.com>
2023-03-05 19:01:42 +0100

    removed cviko1

--- cviko1/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-"c:\Program Files\Java\jdk1.8.0_231\bin\java" -Djava.library.path="bin;." -Xms64m -Xmx512m -classpath "xith.jar;." Scene
-pause
--- cviko1/serialization/compileMarshall.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-"c:\Program Files\Java\jdk1.8.0_231\bin\javac" Marshall.java
-pause
